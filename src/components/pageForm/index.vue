daf975e87d081eaf5d71f968a6c8d3d0236e6188
PrettyZhan <1272486734@qq.com>
2019-05-05 19:04:08 +0800

    更新form组件的功能

--- src/components/pageForm/index.vue
+++ src/components/pageForm/index.vue
@@ -1,14 +1,18 @@
 <template>
-  <pageScroll>
-    <el-form
-      class="page-form"
-      label-width="100px"
-      size="medium">
-      <el-form-item label="名称">
-        <el-input></el-input>
-      </el-form-item>
-    </el-form>
-  </pageScroll>
+  <div class="page-form">
+    <pageScroll class="page-form__main">
+      <el-form
+        label-width="100px"
+        size="medium">
+        <el-form-item label="名称">
+          <el-input ></el-input>
+        </el-form-item>
+      </el-form>
+    </pageScroll>
+    <div class="page-form__footer">
+      <el-button type="primary" size="small" :round="false">保存</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -19,22 +23,53 @@ export default {
   components: {
     pageScroll
   },
+  props: {
+    forms: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
   data () {
     return {
+      formData: {},
+      formRule: []
     }
   }
 }
 </script>
 
 <style lang="less">
+  @import '../../assets/style/custom';
+
   .page-form {
-    text-align: center;
+    position: relative;
+    width: 100%;
+    height: 100%;
+    padding-bottom: 40px;
     padding-top: 40px;
     .el-form-item {
       display: inline-block;
     }
     .el-form-item__content {
       min-width: 400px;
+    }
+    .page-form__main {
+      text-align: center;
+      height: 100%;
+    }
+    .page-form__footer {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      padding-top: 6px;
+      text-align: right;
+      border-top: 1px solid @baseBorderColor;
+      .el-button {
+        min-width: 100px;
+      }
     }
   }
 </style>
--- src/pages/information/blog/blogForm.vue
+++ src/pages/information/blog/blogForm.vue
@@ -18,6 +18,49 @@ export default {
   },
   data () {
     return {
+      forms: {
+        name: {
+          rules: [
+            {require: true, message: '请输入名称', trigger: 'click'}
+          ],
+          label: '活动名称',
+          type: 'input',
+          value: ''
+        },
+        region: {
+          rules: [
+            {require: true, message: '请选择活动区域', trigger: 'change'}
+          ],
+          label: '活动区域',
+          type: 'select',
+          value: '',
+          selects: [
+            {label: '区域1', value: 'quyu1'},
+            {label: '区域2', value: 'quyu2'}
+          ]
+        },
+        datetime: {
+          rules: [
+            {require: true, message: '请选择活动时间', trigger: 'change'}
+          ],
+          label: '活动时间',
+          type: 'date-picker',
+          value: ''
+        },
+        delivery: {
+          label: '即时配送',
+          type: 'switch',
+          value: false
+        },
+        type: {
+          rules: [
+            {type: 'array', require: true, message: '请选择至少一个活动性质', trigger: 'change'}
+          ],
+          label: '活动性质',
+          type: 'checkbox',
+          value: []
+        }
+      }
     }
   }
 }
